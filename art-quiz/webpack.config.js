41aa5bb86f96e951d883209619297f26165afc6a
ivan-nesusanin <l75x5@yandex.ru>
2021-11-16 23:52:54 +0300

    feat: add webpack bundle, eslint, initial layout: index.html, main.scss

---
+++ art-quiz/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 4200,
+    open: {
+      app: {
+        name: 'google-chrome',
+      },
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      //   {
+      //     test: /\.css$/,
+      //     use: [MiniCssExtractPlugin.loader,'style-loader', 'css-loader']
+      //   },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
